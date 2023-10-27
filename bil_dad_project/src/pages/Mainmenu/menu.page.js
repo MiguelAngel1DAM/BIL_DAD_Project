a221dcd7e42347d424e5485a7e5be42671ecc47e
MiguelAngel1DAM <miguelangelhernandezgarcia@alumno.ieselrincon.es>
2023-10-27 17:46:07 +0100

    main page

--- bil_dad_project/src/pages/Mainmenu/menu.page.js
+++ bil_dad_project/src/pages/Mainmenu/menu.page.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Badge } from 'antd';
-import React, { useState } from 'react';
 import './style.css';
 import { BiUserCircle } from 'react-icons/bi';
 import Footermenu from '../../components/Footermenu/Footermenu';
@@ -16,15 +15,15 @@ function Menu() {
       <div className='menus'>
         <div className='serversMenu'>
           <h2>Servers</h2>
-        <button> Server1</button>
-        <button> Server2</button>
-        <button> Server3</button>
+        <button><Badge count={'on'} color='green' className='badgeserver'/> Server1</button>
+        <button><Badge count={'off'} color='red' className='badgeserver'/> Server2</button>
+        <button><Badge count={'loading'} color='orange' className='badgeserver'/>Server3</button>
         </div>
         <div className='databasesMenu'>
           <h2>Databases</h2>
-        <button> base1</button>
-        <button> base2</button>
-        <button> base3</button>
+          <button><Badge count={'on'} color='green'className='badgebase'/> Database1</button>
+        <button><Badge count={'off'} color='red' className='badgebase'/> Database2</button>
+        <button><Badge count={'loading'} color='orange' className='badgebase'/> Database3</button>
         </div>
       </div>
       <Footermenu/>
